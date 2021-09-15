aebdb2d3bdb11fc846fa080eb5b1ce952399f559
Luis Vice <khanccer@gmail.com>
2021-09-15 12:01:46 -0500

    task v0.1

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+
+  listTareas : Tarea[] = []
+  nombreTarea = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarTarea(){
+
+    if ( this.nombreTarea.length != 0){
+      
+    const tarea : Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
+
+    
+      this.listTareas.push(tarea)
+    }
+
+    this.nombreTarea = '';
+
+  }
+
+  eliminarTarea(index: number): void{
+    this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(tarea: Tarea, index: number): void {
+    this.listTareas[index].estado = !tarea.estado;
+  }
+
+}
